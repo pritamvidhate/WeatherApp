254f9609d572963fbd0444b986455abf6cfde607
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2021-02-14 15:16:15 +0530

    changes are done

--- public/Js/main.js
+++ public/Js/main.js
@@ -17,8 +17,8 @@ const getInfo = async(event) =>{
 
     }else{
         try{
-            let url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=a98f40fcfd3e2469d7bd0b88eafc7aed`
-            const response = await fetch(url);
+            
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=a98f40fcfd3e2469d7bd0b88eafc7aed`);
             const data = await response.json ();
             const arrData = [data];
             
